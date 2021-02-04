24a287bb14485df4f49db6f6530fa81d15aa966c
ayush-k-lang <77571966+ayush-k-lang@users.noreply.github.com>
2021-02-04 14:40:43 +0530

    Add files via upload

---
+++ balcony_2.js
@@ -0,0 +1,46 @@
+var picture,status;
+var objects=[];
+
+function preload(){
+picture=loadImage("image 4.jpg");
+}
+
+function setup(){
+canvas=createCanvas(800,600);
+canvas.position(550,200);
+objectDetector=ml5.objectDetector('cocossd',modelloaded);
+document.getElementById("status_button").innerHTML="Status : Detecting objects";
+}
+
+function draw(){
+image(picture,0,0,800,600);
+
+if(status != ""){
+for(var i = 0 ; i < objects.length ; i++){
+document.getElementById("status_button").innerHTML="Status : Objects Detected";
+fill("blue");
+percent=floor(objects[i].confidence*100);
+text(objects[i].label + " " + percent + "%" , objects[i].x+15 , objects[i].y+15);
+noFill();
+stroke("red");
+rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height); 
+}
+}
+
+}
+
+function modelloaded(){
+    console.log("modelloaded");
+    status=true;
+    objectDetector.detect(picture,gotResult);
+}
+
+function gotResult(error,result){
+    if(error){
+console.log("error"+error);
+    }
+    else{
+console.log(result);
+objects=result;
+    }
+}
---
+++ homepage.js
@@ -0,0 +1,15 @@
+function balcony_1(){
+    windows.location="balcony_1.html";
+}
+function balcony_2(){
+    windows.location="balcony_2.html";
+}
+function balcony_3(){
+    windows.location="balcony_3.html";
+}
+function decoration_corner(){
+    windows.location="decoration_corner.html";
+}
+function computer_room(){
+    windows.location="computer_room.html";
+}
